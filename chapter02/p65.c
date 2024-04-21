ea51f4a8d3171b6146eb038e639dea240f59cff6
steveienciu <steveienciu@yahoo.ca>
2024-04-21 19:50:04 -0400

    update

--- chapter02/p65.c
+++ chapter02/p65.c
@@ -24,11 +24,13 @@ int main()
 /* Return 1 when x contains an odd number of 1s; 0 otherwise. Assume w=32 */
 int odd_ones(uint32_t x)
 {
+	/* take xor of each half of bit string to get rid of 1's in the "same" spot */
 	x ^= x >> 16;
 	x ^= x >> 8;
 	x ^= x >> 4;
 	x ^= x >> 2;
 	x ^= x >> 1;
 
+	/* take and to have the result be either a 1 or 0 */
 	return x & 0x1;
 }
