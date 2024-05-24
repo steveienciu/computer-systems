ec3a30cfabece19f7aee00e7cd5ee5507606f14e
steveienciu <steveienciu@yahoo.ca>
2024-05-24 18:20:42 -0400

    update

--- chapter03/p62.c
+++ chapter03/p62.c
@@ -1,3 +1,32 @@
+/*
+finish the switch statement using the following assembly code
+.L8: ; MODE_E
+ movl $27, %eax
+ ret
+.L3: ; MODE_A
+ movq (%rsi), %rax
+ movq (%rdi), %rdx
+ movq %rdx, (%rsi)
+ ret
+.L5: ; MODE_B
+ movq (%rdi), %rax
+ addq (%rsi), %rax
+ movq %rax, (%rdi)
+ ret
+.L6: ; MODE_C
+ movq $59, (%rdi)
+ movq (%rsi), %rax
+ ret
+.L7: ; MODE_D
+ movq (%rsi), %rax
+ movq %rax, (%rdi)
+ movl $27, %eax
+ ret
+.L9: ;default
+ movl $12, %eax
+ ret
+*/
+
 /* Enumerated type creates set of constants numbered 0 and upward */
 typedef enum {MODE_A, MODE_B, MODE_C, MODE_D, MODE_E} mode_t;
 
