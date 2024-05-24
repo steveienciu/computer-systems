befb3a457cb560e839d8663cd2bd544d009c4b20
steveienciu <steveienciu@yahoo.ca>
2024-05-24 08:44:06 -0400

    new code

---
+++ chapter03/p60.c
@@ -0,0 +1,30 @@
+/* write C code for following assembly code
+long loop(long x, int n)
+x in %rdi, n in %esi
+loop:
+ movl %esi, %ecx
+ movl $1, %edx
+ movl $0, %eax
+ jmp .L2
+ .L3:
+ movq %rdi, %r8
+ andq %rdx, %r8
+ orq %r8, %rax
+ salq %cl, %rdx
+ .L2:
+ testq %rdx, %rdx
+ jne .L3
+ rep; ret
+ */
+
+ long loop(long x, int n)
+ {
+	 long result = 0;
+	 long mask;
+
+	 for (mask = 1; mask != 0; mask <<= n) {
+		 result |= (mask & x);
+	 }
+
+	 return result;
+}
