164593e673c9091cff2922ea2e38a2a2111138ed
steveienciu <stevenienciu@yahoo.ca>
2025-06-12 13:43:00 +0000

    update

--- chapter09/p17/mm.h
+++ chapter09/p17/mm.h
@@ -1,5 +1,9 @@
 /* mm.h */
 
+/* Added to header file for testing purposes */
+extern char *next_fitp;
+extern char *heap_listp;
+
 extern int mm_init(void);
 extern void *mm_malloc (size_t size);
 extern void mm_free (void *ptr);
